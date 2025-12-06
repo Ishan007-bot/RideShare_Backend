276a369561b64ac02a623c3d287eb0b9f6653ee2
Ishan <ishanganguly10a12@gmail.com>
2025-12-06 21:16:59 +0530

    Fix: Add @EnableMongoRepositories to resolve repository bean issue

--- src/main/java/com/ishan/RideShare/UberRideShareApplication.java
+++ src/main/java/com/ishan/RideShare/UberRideShareApplication.java
@@ -2,8 +2,10 @@ package com.ishan.RideShare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.ishan")
+@EnableMongoRepositories(basePackages = "com.ishan.repository")
 public class UberRideShareApplication {
 
 	public static void main(String[] args) {
